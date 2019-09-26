0493439f4e65dda655076d816fb5c07f6dab9553
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-09-26 18:15:19 +0100

    Wiring Up Sessions

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -8,10 +8,14 @@ router.get('/login', function (req, res) {
 });
 router.post('/login', function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    if (email) {
-        res.send(email.toUpperCase());
+    //type guard
+    if (email && password && email === 'hi@there.com' && password === 'password') {
+        //mark this person as logged in
+        req.session = { loggedIn: true };
+        //redirect them to the root
+        res.redirect('/');
     }
     else {
-        res.send('You must provide an email');
+        res.send('Invalid email or password');
     }
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -25,10 +25,17 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email.toUpperCase());
+  //type guard
+  if (email && password && email === 'hi@there.com' && password === 'password') {
+
+    //mark this person as logged in
+    req.session = { loggedIn: true };
+
+    //redirect them to the root
+    res.redirect('/');
+
   } else {
-    res.send('You must provide an email');
+    res.send('Invalid email or password');
   }
 
 });
